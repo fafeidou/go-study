0443ade9d21cb5d7819ad48bdca212a8ba9d90ad
qfx <qinfuxiang>
2020-02-02 17:16:21 +0800

    add slice

---
+++ slice/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//append()添加元素和切片扩容
+	var numSlice []int
+	for i := 0; i < 10; i++ {
+		numSlice = append(numSlice, i)
+		fmt.Printf("%v  len:%d  cap:%d  ptr:%p\n", numSlice, len(numSlice), cap(numSlice), numSlice)
+	}
+}
